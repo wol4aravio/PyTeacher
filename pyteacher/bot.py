cd2d3c3c3f2ca303941828bbfd3fe2538f8ea16c
wol4aravio <panovskiy.v@yandex.ru>
2021-03-18 14:33:09 +0300

    done

--- pyteacher/bot.py
+++ pyteacher/bot.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot(os.environ.get("TELEGRAM_BOT_TOKEN"))
 
 @bot.message_handler(commands=["start"])
 def handle_command(message):
-    bot.reply_to(message, "Hello, welcome to PyTeacher Bot!")
+    bot.send_message(message.chat.id, "Hello, welcome to PyTeacher Bot!")
 
 
 bot.polling()
