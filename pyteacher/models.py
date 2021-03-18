02f7225bb5bf471f76b695d5376aad6070d50280
wol4aravio <panovskiy.v@yandex.ru>
2021-03-18 17:24:22 +0300

    date joined

--- pyteacher/models.py
+++ pyteacher/models.py
@@ -1,8 +1,11 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 
 
 class User(BaseModel):
     telegram_user_id: str
+    date_joined: datetime = datetime.utcnow()
     repo_url: str = None
     access_token: str = None
     is_admin: bool = False
