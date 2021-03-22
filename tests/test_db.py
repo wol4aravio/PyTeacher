20a56f2837941b55c2c37a7823b85bd9d05be65e
wol4aravio <panovskiy.v@yandex.ru>
2021-03-22 10:13:47 +0300

    tests

---
+++ tests/test_db.py
@@ -0,0 +1,11 @@
+from pyteacher.db import PyTeacherDB
+from pyteacher.models import User
+
+
+def test_db():
+    user_1 = User(telegram_user_id="1")
+    with PyTeacherDB(db="dev") as db:
+        assert db.add_user(user_1)
+        assert not db.add_user(user_1)
+        assert db.remove_user(user_1.telegram_user_id)
+        db._mongo.drop_database("dev")  # pylint: disable=protected-access
